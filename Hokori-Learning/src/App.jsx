a9f1dc50c4484aaaacf1b20a4577bd12eee2e21d
danh0999 <dangducanh999@gmail.com>
2025-10-08 23:37:27 +0700

    vercel-config

--- Hokori-Learning/src/App.jsx
+++ Hokori-Learning/src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
-import { Provider } from "react-redux";  
+import { Provider } from "react-redux";
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes/Route.jsx";
 import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
   const router = createBrowserRouter(routes);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <RouterProvider router={router} />
+        <SpeedInsights />
       </PersistGate>
     </Provider>
   );
