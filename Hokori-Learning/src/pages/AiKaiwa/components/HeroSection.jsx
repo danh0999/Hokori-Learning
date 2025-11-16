a84c6bb96a3c848c47aa569a75d4275b26f389e2
danh0999 <dangducanh999@gmail.com>
2025-11-16 19:31:36 +0700

    AI kaiwa

--- Hokori-Learning/src/pages/AiKaiwa/components/HeroSection.jsx
+++ Hokori-Learning/src/pages/AiKaiwa/components/HeroSection.jsx
@@ -7,11 +7,12 @@ const HeroSection = () => {
     <section className={styles.heroSection}>
       <div className={styles.inner}>
         <div className={styles.iconWrap}>
-          <RiRobot2Line className={styles.icon} />
+          <i className={styles.icon + " fa-solid fa-microphone"}></i>
         </div>
+
         <h1 className={styles.title}>Nhận diện giọng nói (AI Kaiwa)</h1>
         <p className={styles.subtitle}>
-          Luyện nói tiếng Nhật và nhận phản hồi phát âm tức thì
+          Luyện nói tiếng Nhật và nhận phản hồi phát âm tức thì.
         </p>
       </div>
     </section>
